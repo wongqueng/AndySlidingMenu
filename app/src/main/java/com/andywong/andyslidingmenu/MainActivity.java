545d2d0f6ac4c36834e3c848f8b4052c9df6e21b
andywong <a1146430762@163.com>
2016-07-24 23:29:51 +0800

    包结构重构

--- app/src/main/java/com/andywong/andyslidingmenu/MainActivity.java
+++ app/src/main/java/com/andywong/andyslidingmenu/MainActivity.java
@@ -1,13 +1,34 @@
 package com.andywong.andyslidingmenu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.andywong.views.viewgroup1;
 
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+    viewgroup1 menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sliding_menu);
+        menu = (viewgroup1) findViewById(R.id.menulayout);
+        TextView tx1 = (TextView) findViewById(R.id.tx1);
+        TextView tx2 = (TextView) findViewById(R.id.tx2);
+        tx1.setOnClickListener(this);
+        tx2.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tx1:
+                menu.setType(1);
+                break;
+            case R.id.tx2:
+                menu.setType(0);
+                break;
+
+        }
     }
 }
